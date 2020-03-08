6d22aff118630b657e07fa8a31d7b3630d8b224e
TatuFlorin <sk.florin@yahoo.com>
2020-03-08 10:57:12 +0200

    Init project.

---
+++ src/jkMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jkMovie.Startup))]
+namespace jkMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
